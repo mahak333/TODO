e06b1f2c4e3bb566193c1565fbc45ed9f72e75e5
Mahak <patlemahak88@gmail.com>
2026-02-21 10:29:58 +0530

    Create TodoList component for displaying todos

---
+++ frontend/src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TodoList = ({ todos }) => {
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
